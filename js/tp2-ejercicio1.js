de3656339a10ccb59285c430ae72ec5f00de1889
Lucas Capdevila <lcapdevila60@gmail.com>
2023-10-28 15:25:15 -0300

    ejercicio 1 resuelto

---
+++ js/tp2-ejercicio1.js
@@ -0,0 +1,14 @@
+/*
+1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que
+ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
+*/
+
+let edadUsuario = prompt('Ingrese su edad')
+
+if (edadUsuario >= 18 && edadUsuario <= 99) {
+  document.write('Usted ya puede conducir !')
+} else if(edadUsuario > 0 && edadUsuario <= 17){
+  document.write('Usted es demasiado joven para conducir')
+} else{
+  document.write('El número ingresado no es válido')
+}
