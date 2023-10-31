3ef76c1336522c994beb66ea3d1f56d5c694a501
Lucas Capdevila <lcapdevila60@gmail.com>
2023-10-30 22:28:52 -0300

    se resuelve ejercicio 6

---
+++ js/tp2-ejercicio6.js
@@ -0,0 +1,18 @@
+/*
+6- Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
+
+1
+22
+333
+4444
+55555
+666666
+…….
+*/
+
+for(let filas = 1; filas <= 30; filas++){
+  for(let contador = 0; contador < filas; contador++){
+    document.write(filas)
+  }
+  document.write('<br>')
+}
