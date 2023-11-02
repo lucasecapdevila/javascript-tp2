c1b2d2521bd15d32eaf93a32a4fe1e51c9fc11d2
Lucas Capdevila <lcapdevila60@gmail.com>
2023-11-01 22:53:38 -0300

    se resuelve ejercicio 13

---
+++ js/tp2-ejercicio13.js
@@ -0,0 +1,9 @@
+//* Ejercicios con String
+/*
+13- Realiza un script que pida un texto y lo muestre en mayúsculas.
+*/
+
+let texto = prompt('Ingrese un texto (será convertido a mayúsculas)')
+
+texto = texto.toUpperCase()
+document.write(texto)
